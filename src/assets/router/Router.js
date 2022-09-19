6cab7cd97f2cdaf86419000f7eaf73caef41d101
Marco Franca <dev.marcotullio@gmail.com>
2022-09-19 13:13:22 -0300

    vercel test changes[TEST]

--- src/assets/router/Router.js
+++ src/assets/router/Router.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import PageNotFound from "../../pages/pageNotFound/PageNotFound.jsx";
+import PageNotFound from "../../pages/error/PageNotFound.jsx";
 import Home from "../../pages/home/Home";
 
 export default function Router() {
