1e0cda1a73304bb8873160862cf2a494374932fd
Marco Franca <dev.marcotullio@gmail.com>
2022-09-19 10:43:25 -0300

    inclusion of the image in the page not found page

--- src/pages/PageNotFound/PageNotFound.jsx
+++ src/pages/PageNotFound/PageNotFound.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import ErrorImg from "../../assets/images/ups-erro-404.jpg"
 
 export default function PageNotFound() {
     return (
         <>
             <div>
                 <h1>Page is not found - 404</h1>
-                <img src="./image/ups-erro-404.jpg" alt="Page is not found - 404"/>
+                <img src={ErrorImg} alt="Page is not found - 404"/>
             </div>
         </>
     )
