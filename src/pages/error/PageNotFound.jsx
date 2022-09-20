af29635df89602398c103e89f12166eb173b02dc
Marco Franca <dev.marcotullio@gmail.com>
2022-09-20 09:57:58 -0300

    styled components tags have been added for styling[UPDATE]

--- src/pages/error/PageNotFound.jsx
+++ src/pages/error/PageNotFound.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import ErrorImg from "../../assets/images/ups-erro-404.jpg"
+import {Div, Main} from "./PageNotFoundStyle";
 
 export default function PageNotFound() {
     return (
-        <>
-            <div>
-                <h1>Page is not found - 404</h1>
-                <img src={ErrorImg} alt="Page is not found - 404"/>
-            </div>
-        </>
+        <Main>
+            <Div>
+                <h1 className='error-title'>Not Found</h1>
+                <p className='error-text'>The resouce request cold not be found on this server!</p>
+            </Div>
+            <img src={ErrorImg} alt="Page is not found - 404"/>
+        </Main>
     )
 }
