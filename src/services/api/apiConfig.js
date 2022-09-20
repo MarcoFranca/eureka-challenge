d038f520939e3d5b75f7d961ca60541a4973e364
Marco Franca <dev.marcotullio@gmail.com>
2022-09-20 09:40:43 -0300

    Bing api configuration[UPDATE]

--- src/services/api/apiConfig.js
+++ src/services/api/apiConfig.js
@@ -4,3 +4,7 @@
 export const fieloUrl = 'https://challenge-fielo.herokuapp.com'
 
 export const fieloKey = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCh7uxHjWd1CyRgPD4XHcIPKiDb"
+
+//Bing API
+
+export const bingUrl = 'https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=pt-BR'
