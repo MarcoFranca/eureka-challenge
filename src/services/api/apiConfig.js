c7f7b067d5d7c529081be3dab9c96d7afc0414dd
Marco Franca <dev.marcotullio@gmail.com>
2022-09-21 13:26:39 -0300

    Fielo api settings update [UPDATE]

--- src/services/api/apiConfig.js
+++ src/services/api/apiConfig.js
@@ -1,10 +1,27 @@
 // *************  API keys and URLs    ***************
 
-//fielo API
+//Fielo access API
 export const fieloUrl = 'https://challenge-fielo.herokuapp.com'
 
 export const fieloKey = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCh7uxHjWd1CyRgPD4XHcIPKiDb"
 
-//Bing API
+//Fielo methods
 
+//**Endpoint responsible for returning all users.**
+export const apiUsers = "/users"
+//**Endpoint responsible for returning the user profile.**
+export const apiId = "/users/id"
+//**Endpoint responsible for returning user activities.**
+export const apiActivities = "/users/id/activities"
+
+
+//**Endpoint responsible for returning all programs.**
+export const apiProgramsId = "/programs/id"
+//**Endpoint responsible for returning all programs.**
+export const apiProgramsLevels = "/programs/id/levels"
+//**Endpoint responsible for returning all programs.**
+export const apiLevels = "/levels/id"
+
+//Bing API
 export const bingUrl = 'https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=pt-BR'
+
