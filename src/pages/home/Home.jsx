3d02a860eeba0f0a017e181f21d8f10c0d7a0423
Marco Franca <dev.marcotullio@gmail.com>
2022-09-21 13:37:45 -0300

    Main was added [UPDATE]

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../../components/Header/Header";
+import Main from "../../components/main/Main";
 
 export default function Home() {
     return (
-        <div>
+        <>
             <Header/>
-        </div>
+            <Main/>
+        </>
     )
 }
