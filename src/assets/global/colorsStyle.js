822f7c150a02658e8ae7ce958cbf8681104f7fdd
Marco Franca <dev.marcotullio@gmail.com>
2022-09-21 13:35:23 -0300

    new colors update[UPDATE]

--- src/assets/global/colorsStyle.js
+++ src/assets/global/colorsStyle.js
@@ -1,5 +1,20 @@
 export const colors = {
+    nightBackground : 'rgba(0, 0, 0, 1)',
     colorGray : 'rgba(194, 195, 196, 0.63)',
     colorAlert: 'rgb(255,0,0,1)',
-    nightBackground : 'rgba(0, 0, 0, 1)'
+    colorWhite : 'rgba(255, 255, 255, 1)',
+
+// scroll colors
+
+    scrollbarColor: 'rgba(250,251,253,1)',
+    scrollBackgorundColor: 'rgba(215, 219, 231, 1)',
+    scrollBorderColor: 'rgba(250,251,253,1)',
+
+//learderboard colors
+
+    leaderboardHooverColor: 'rgba(240,248,252,1)',
+    leaderboardTitleColor: 'rgba(138,148,166,1)',
+    leaderboardFontColor: 'rgba(22, 50, 92, 1)'
+
 }
+
