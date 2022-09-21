fa6f96448101a1de6bf43d90eb841b03ebd96444
Marco Franca <dev.marcotullio@gmail.com>
2022-09-21 13:36:47 -0300

    change in page width [PATCH]

--- src/components/Header/HeaderStyled.js
+++ src/components/Header/HeaderStyled.js
@@ -3,10 +3,10 @@ import {colors} from "../../assets/global/colorsStyle";
 
 export const HeaderStyled = styled.header`
   height: 5.25rem;
-  width: 100vw;
   box-sizing: border-box;
   padding: 1.5rem 1.5rem;
   box-shadow: 0 2px 3px ${colors.colorGray};
+  position: relative;
   
   @media screen and (min-width: 768px){
     padding: 1.5rem 4.5rem;
