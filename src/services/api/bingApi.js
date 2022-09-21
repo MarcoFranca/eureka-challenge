54ca1c49b30b08da6caa6519604c386e1232519a
Marco Franca <dev.marcotullio@gmail.com>
2022-09-21 13:58:07 -0300

    removed unused line of code [DELL]

--- src/services/api/bingApi.js
+++ src/services/api/bingApi.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 import {bingUrl} from "./apiConfig";
 
-export const  getBackground = (setState, setTitle) => {
+export const  getBackground = () => {
     axios.get(`${bingUrl}`)
     .then(function (response) {
-        setTitle(response.data.images[0].title)
         sessionStorage.setItem('urlImg',`https://www.bing.com${response.data.images[0].url}`)
     })
    .catch(function (error) {
