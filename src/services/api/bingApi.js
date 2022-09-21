3f6003ceea150b85f4f9f6a43885edefd28a08bf
Marco Franca <dev.marcotullio@gmail.com>
2022-09-21 13:34:47 -0300

    inclusion of configuration for organization of the bing api[UPDATE]

--- src/services/api/bingApi.js
+++ src/services/api/bingApi.js
@@ -1,19 +1,13 @@
 import axios from "axios";
+import {bingUrl} from "./apiConfig";
 
-const options = {
-    origin: 'http://localhost:3000',
-    method: 'get',
-    url: 'https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=pt-BR',
-};
-
-export const  getBackground = async () => { await axios.request(options)
+export const  getBackground = (setState, setTitle) => {
+    axios.get(`${bingUrl}`)
     .then(function (response) {
-
-        let urlImg = 'https://www.bing.com'+ response.data.images[0].url
-        localStorage.setItem('urlImg',urlImg)
-
-
-    }).catch(function (error) {
-        console.error(error);
+        setTitle(response.data.images[0].title)
+        sessionStorage.setItem('urlImg',`https://www.bing.com${response.data.images[0].url}`)
     })
+   .catch(function (error) {
+        console.error(error);
+   })
 }
