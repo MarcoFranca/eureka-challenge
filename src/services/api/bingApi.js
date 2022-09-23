19162eb40dfef5737e2928f43fc5560f622b42fd
Marco Franca <dev.marcotullio@gmail.com>
2022-09-23 20:31:28 -0300

    title capture to image alt [UPDATE]

--- src/services/api/bingApi.js
+++ src/services/api/bingApi.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 import {bingUrl} from "./apiConfig";
 
-export const  getBackground = (setURL) => {
+export const  getBackground = (setURL, setTitle) => {
     axios.get(bingUrl)
     .then((response) => {
         setURL(`https://www.bing.com${response.data.images[0].url}`)
+        setTitle(response.data.images[0].copyright)
     })
    .catch(function (error) {
         console.error(error);
