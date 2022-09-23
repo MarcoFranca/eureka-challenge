f1b5c62262a997afd337454d1f4c86617d35bee5
Marco Franca <dev.marcotullio@gmail.com>
2022-09-22 21:01:13 -0300

    Improved API connection function [UPDATE]

--- src/services/api/bingApi.js
+++ src/services/api/bingApi.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 import {bingUrl} from "./apiConfig";
 
-export const  getBackground = () => {
-    axios.get(`${bingUrl}`)
-    .then(function (response) {
-        sessionStorage.setItem('urlImg',`https://www.bing.com${response.data.images[0].url}`)
+export const  getBackground = (setURL) => {
+    axios.get(bingUrl)
+    .then((response) => {
+        setURL(`https://www.bing.com${response.data.images[0].url}`)
     })
    .catch(function (error) {
         console.error(error);
