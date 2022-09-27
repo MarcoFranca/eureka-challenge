44c74db8df8c9b434befa34b8d923189b53fe9d7
Marco Franca <dev.marcotullio@gmail.com>
2022-09-27 07:06:51 -0300

    Added use context and openOverview function [UPDATE]

--- src/components/leaderboardCell/LeaderboardCell.jsx
+++ src/components/leaderboardCell/LeaderboardCell.jsx
@@ -1,15 +1,25 @@
-import React from "react";
+import React, {useContext} from "react";
 import {ButtonLeaderboard} from "./LeaderboardCellStyle";
+import {openOverview} from "../../assets/global/functions";
+import {UsersContext} from "../../context/UsersContext";
+
+//user cell to be created inside the leaderboard card
 
 export default function LeaderboardCell(props) {
+    const {setUserId} = useContext(UsersContext)
 
     return (
-        <ButtonLeaderboard>
-
-            <p>{props.position}</p>
-            <div><img src={props.image} alt={props.alt}/>{props.name}</div>
-            <p>{props.points}</p>
-
+        <ButtonLeaderboard onClick={()=> {
+            openOverview(
+                props.id,
+                "overviewCard",
+                setUserId)}}
+        >
+            <div id={props.id} className="leaderContainer">
+                <p>{props.position}</p>
+                <div><img src={props.image} alt={props.alt}/>{props.name}</div>
+                <p>{props.points}</p>
+            </div>
         </ButtonLeaderboard>
     )
 }
