26618ae0f36cd78faafac63698a092ac6796e985
Marco Franca <dev.marcotullio@gmail.com>
2022-09-30 08:31:21 -0300

    functions to generate the bing token and image + the return of cards with leaderboard, user profile and user activity feed [UPDATE]

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,18 +1,58 @@
-import React, {useEffect} from "react";
-import {getBackground} from "../../services/api/bingApi";
+import React, {useEffect, useState} from "react";
 import {MainStyle} from "./MainStyle";
-import CardLeaderboard from "../cardLeaderborad/CardLeaderboard";
+
+// ***********  Componentes  *******************
+
+import LeaderboardCard from "../leaderboardCard/LeaderboardCard";
+import UserProfile from "../userProfile/UserProfile";
+import FeedCard from "../feedCard/FeedCard";
+
+// ***********  API  *******************
+
+import {getBackground} from "../../services/api/bingApi";
+import {getFielo, tokenGenerator} from "../../services/api/apiFielo";
+import {fielo} from "../../services/api/apiConfig";
+
+// ***********  Context  *******************
+
+import {useContext} from "react";
+import {UsersContext} from "../../context/UsersContext";
 
 
+// *********** Main page responsible for calling the start functions of the *******************
+// *********** apis and build leaderboard, user Profile and feed cards *******************
 
 export default function Main() {
+    const {token, setToken, setUsers} = useContext(UsersContext)
+    const [urlImg, setUrlimg] = useState()
+    const [urlTitleImg, setTitleImg] = useState()
 
-    useEffect(()=>
-        getBackground()
-    )
+
+// ********* Function calls responsible for creating the faithful Fielo API token *******************
+// ********* and the Bing API background image *******************
+
+    useEffect(()=>{
+        tokenGenerator(setToken)
+        getBackground(setUrlimg,setTitleImg)
+    },[])
+
+// *********** Function responsible for capturing API users *******************
+
+    useEffect(()=>{
+        token.length > 0 && getFielo(token,fielo.urlUsers,setUsers)
+    },[token, setUsers])
+
     return (
         <MainStyle>
-            <CardLeaderboard/>
+            <img className={'background'} src={urlImg} alt={urlTitleImg}/>
+
+            <LeaderboardCard/>
+
+            <UserProfile/>
+
+            <FeedCard/>
+
         </MainStyle>
     )
 }
+
