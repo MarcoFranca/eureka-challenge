371361c67928b6bf1e6bad7b55d82439a21c9f6c
Marco Franca <dev.marcotullio@gmail.com>
2022-10-01 17:05:59 -0300

    context was added [UPDATE]

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,15 @@
 import React from "react";
-import Header from "../../components/Header/Header";
+import Header from "../../components/header/Header";
 import Main from "../../components/main/Main";
+import {UsersContextProvider} from "../../context/UsersContext";
 
 export default function Home() {
     return (
         <>
             <Header/>
-            <Main/>
+            <UsersContextProvider>
+                <Main/>
+            </UsersContextProvider>
         </>
     )
 }
