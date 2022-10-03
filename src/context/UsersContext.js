9b4d67feb9e4458e69138f683570d189014682c3
Marco Franca <dev.marcotullio@gmail.com>
2022-10-03 19:57:39 -0300

    user context [ADD]

---
+++ src/context/UsersContext.js
@@ -0,0 +1,39 @@
+import {createContext, useState} from "react";
+
+export const UsersContext = createContext('')
+
+export const UsersContextProvider = ({children})=>{
+    const [token, setToken] = useState([])
+
+    const [userId,setUserId] = useState([])
+    const [progId,setProgId] = useState([])
+    const [levelId,setLevelId] = useState([])
+
+    const [users,setUsers] = useState([])
+    const [userSelected, setUserSelected] = useState([])
+    const [userProfile, setUserProfile] = useState([])
+    const [userPrograms, setUserPrograms] = useState([])
+    const [userLevel, setUserLevel] = useState([])
+    const [userFeed, setUserFeed] = useState([])
+
+
+    return(
+        <UsersContext.Provider value={
+            {
+                token, setToken,
+                users, setUsers,
+                userId, setUserId,
+                userSelected, setUserSelected,
+                userProfile, setUserProfile,
+                userPrograms, setUserPrograms,
+                userLevel, setUserLevel,
+                progId,setProgId,
+                levelId,setLevelId,
+                userFeed, setUserFeed,
+                }}
+        >
+            {children}
+        </UsersContext.Provider>
+    )
+
+}
