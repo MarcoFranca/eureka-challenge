3a96119aaddbf9e198b9f6dbb726ccfd29766f92
Marco Franca <dev.marcotullio@gmail.com>
2022-10-03 17:54:31 -0300

    adjustment of background image size and position for mobiles [UPDATE]

--- src/components/main/MainStyle.js
+++ src/components/main/MainStyle.js
@@ -3,17 +3,13 @@ import {colors} from "../../assets/global/colorsStyle";
 
 export const MainStyle = styled.main`
   height: calc(100vh - 5.25rem);
+  position: relative;
   width: 100%;
   display: flex;
   align-items: center;
   justify-content: center;
   background-color: ${colors.scrollbarColor};
   
-  h1{
-    color: red;
-    z-index: 10;
-  }
-
   .background{
     position: absolute;
     object-fit: cover;
@@ -22,11 +18,11 @@ export const MainStyle = styled.main`
     z-index: 0;
   }
   
-  
   @media screen and (min-width: 1024px){
     justify-content: space-evenly;
+    height: calc(100vh - 5.25rem);
     position: relative;
-    gap: 22px;
+    gap: 1.375rem;
     padding: 0 5%;
     box-sizing: border-box;
   }
